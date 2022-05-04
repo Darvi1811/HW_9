006177c1a1ebbfc66643c921db19174bacc957a0
Darvi <61952350+Darvi1811@users.noreply.github.com>
2022-05-04 16:19:04 +0300

    Add files via upload

---
+++ HW_9/src/com/company/car/Engine.java
@@ -0,0 +1,22 @@
+package com.company.car;
+
+public class Engine {
+    private String model;
+    private int cylinders;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public void setCylinders(int cylinders) {
+        this.cylinders = cylinders;
+    }
+}
